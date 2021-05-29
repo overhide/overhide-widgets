eadc22c3bb1f0ac2e202214911053fd9835a3ed5
Jakub Ner <jakub.at.work@gmail.com>
2021-05-29 14:42:58 -0700

    WIP on appsell component.

---
+++ src/components/appsell/component.ts
@@ -0,0 +1,118 @@
+import {
+  customElement,
+  FASTElement,
+  html,
+  css,
+  attr,
+  observable,
+  Observable,
+  when
+} from "@microsoft/fast-element";
+
+import {
+  Imparter,
+  IOverhideHub,
+  NetworkType,
+  PaymentsInfo,
+  Social
+} from '../hub/definitions';
+
+import w3Css from "../../static/w3.css";
+
+const template = html<OverhideAppsell>`
+  <template class="panel ${e => e.isEnabled ? '' : 'disabled'} ${e => e.orientation}" @click="${e => e.go()}">
+    ${when(e => e.isAuthorized, html<OverhideAppsell>`
+      <slot name="authorized-icon">
+      </slot>
+    `)}
+    ${when(e => !e.isAuthorized, html<OverhideAppsell>`
+      <slot name="unauthorized-icon">
+      </slot>
+    `)}
+    <slot name="header">
+    </slot>
+    <slot name="subheader">
+    </slot>      
+    ${when(e => e.isAuthorized, html<OverhideAppsell>`
+      <slot name="authorized-icon"></slot>
+    `)}
+    ${when(e => !e.isAuthorized, html<OverhideAppsell>`
+      <slot name="unauthorized-icon"></slot>
+    `)}
+  </template>
+`;
+
+const styles = css`
+${w3Css}
+`;
+
+enum Orientation {
+  horizontal = 'horizontal',
+  vertical = 'vertical'
+}
+
+@customElement({
+  name: "overhide-appsell",
+  template,
+  styles,
+})
+export class OverhideAppsell extends FASTElement {
+  @attr 
+  hubId?: string;
+
+  @attr
+  orientation?: Orientation | null = Orientation.vertical;
+
+  @attr
+  priceDollars?: number | null;
+
+  @observable
+  isEnabled?: boolean | null;
+
+  @observable
+  isAuthorized?: boolean | null;
+
+  hub?: IOverhideHub | null; 
+  currentImparter?: Imparter | null;
+
+  public constructor() {
+    super(); 
+  }
+
+  public setHub(hub: any) {
+    this.hub = hub;
+    const notifier = Observable.getNotifier(hub);
+    const that = this;
+    const handler = {
+      handleChange(source: any, propertyName: string) {
+        switch (propertyName) {
+          case 'paymentsInfo':
+            that.paymentInfoChanged(source.paymentsInfo);
+            break;
+        } 
+      }
+    }
+
+    notifier.subscribe(handler, 'paymentsInfo')
+  }
+
+  hubIdChanged(oldValue: string, newValue: string) {
+    const el = document.querySelector(`#${this.hubId}`);
+    if (!el) {
+      console.log(`WARNING: overhide-appsell configured for overhide-hub with ID ${newValue} but no element in DOM with this ID.`);
+      return;
+    }
+    this.setHub(el);
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+  };
+
+  paymentInfoChanged(info: PaymentsInfo): void {
+    this.currentImparter = info.currentImparter;
+  }
+
+  go(): void {
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { OverhideHub } from "./components/hub/component";
 import { OverhideStatus } from './components/status/component'
 import { OverhideLogin } from './components/login/component'
+import { OverhideAppsell } from './components/appsell/component'
 
 OverhideHub;
 OverhideStatus;
 OverhideLogin
+OverhideAppsell;
