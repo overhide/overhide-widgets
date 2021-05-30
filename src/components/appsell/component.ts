89ff0a54c8671800f9147887ae1e71731b623b97
Jakub Ner <jakub.at.work@gmail.com>
2021-05-30 01:02:31 -0700

    Fix up dev-front-end loading of demo-front-end without executing latter scripts

--- src/components/appsell/component.ts
+++ src/components/appsell/component.ts
@@ -20,35 +20,91 @@ import {
 import w3Css from "../../static/w3.css";
 
 const template = html<OverhideAppsell>`
-  <template class="panel ${e => e.isEnabled ? '' : 'disabled'} ${e => e.orientation}" @click="${e => e.go()}">
+  <template class="panel ${e => e.orientation}">
     ${when(e => e.isAuthorized, html<OverhideAppsell>`
-      <slot name="authorized-icon">
+      <slot name="authorized-header">
       </slot>
     `)}
     ${when(e => !e.isAuthorized, html<OverhideAppsell>`
-      <slot name="unauthorized-icon">
+      <slot name="unauthorized-header">
       </slot>
     `)}
-    <slot name="header">
-    </slot>
-    <slot name="subheader">
-    </slot>      
     ${when(e => e.isAuthorized, html<OverhideAppsell>`
-      <slot name="authorized-icon"></slot>
+      <div class="button ${e => e.isEnabled ? '' : 'disabled'}" @click="${e => e.click()}">
+        <slot name="authorized-button">
+          <div class="w3-button button-content">
+            <slot name="authorized-message"></slot>
+          </div>
+        </slot>
+      </div>
     `)}
     ${when(e => !e.isAuthorized, html<OverhideAppsell>`
-      <slot name="unauthorized-icon"></slot>
+      <div class="button ${e => e.isEnabled ? '' : 'disabled'}" @click="${e => e.click()}">
+        <slot name="unauthorized-button">
+          <div class="w3-button button-content">
+            <slot name="unauthorized-prefix"></slot>
+            ${e => e.toDollars(e.topupDollars)}
+            <slot name="unauthorized-postfix"></slot>
+          </div>
+        </slot>
+      </div>
+    `)}
+    ${when(e => e.isAuthorized, html<OverhideAppsell>`
+      <slot name="authorized-footer"></slot>
+    `)}
+    ${when(e => !e.isAuthorized, html<OverhideAppsell>`
+      <slot name="unauthorized-footer"></slot>
     `)}
   </template>
 `;
 
 const styles = css`
 ${w3Css}
+
+  .panel {
+    display: flex;
+  }
+
+  .panel.horizontal {
+    flex-direction: row;
+  }
+
+  .panel.vertical {
+    flex-direction: column;
+  }
+
+  .button {
+    cursor: pointer;
+  }
+
+  .button.disabled {
+    cursor: inherit;
+    opacity: .5;
+  }
+
+  .button-content {
+    width: 100%;
+  }
+
 `;
 
 export enum Orientation {
   horizontal = 'horizontal',
   vertical = 'vertical'
+}
+
+/**
+ * The event sent when an appsell SKU deemed authorized by 
+ * overhide is clicked by the user.
+ * 
+ * Usually safest to route state-changes in response to this
+ * event via the back-end, and validate authorizations.
+ * 
+ * All necessary information to validate is provided in this
+ * event.
+ */
+export interface OverhideSkuClickedEvent {
+
 }
 
 @customElement({
@@ -64,7 +120,13 @@ export class OverhideAppsell extends FASTElement {
   orientation?: Orientation | null = Orientation.vertical;
 
   @attr
+  sku?: string | null;
+
+  @attr
   priceDollars?: number | null;
+
+  @observable
+  topupDollars?: number | null;
 
   @observable
   isEnabled?: boolean | null;
@@ -96,6 +158,9 @@ export class OverhideAppsell extends FASTElement {
     notifier.subscribe(handler, 'paymentsInfo')
   }
 
+  public click(): void {
+  }
+
   hubIdChanged(oldValue: string, newValue: string) {
     const el = document.querySelector(`#${this.hubId}`);
     if (!el) {
@@ -113,6 +178,7 @@ export class OverhideAppsell extends FASTElement {
     this.currentImparter = info.currentImparter;
   }
 
-  public go(): void {
+  toDollars(what?: number | null): string {
+    return (Math.round((what || 0) * 100) / 100).toFixed(2);
   }
 }
