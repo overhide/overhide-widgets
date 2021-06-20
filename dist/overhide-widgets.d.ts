3e2bc7df8ae09ca53657c3e697e8adf0791db65c
Jakub Ner <jakub.at.work@gmail.com>
2021-06-19 17:31:59 -0700

    Add TSX types

--- dist/overhide-widgets.d.ts
+++ dist/overhide-widgets.d.ts
@@ -3,4 +3,14 @@ export * from "./components/hub/definitions";
 export * from "./components/status/component";
 export * from "./components/login/component";
 export * from "./components/appsell/component";
+declare global {
+    namespace JSX {
+        interface IntrinsicElements {
+            'overhide-hub': any;
+            'overhide-appsell': any;
+            'overhide-status': any;
+            'overhide-login': any;
+        }
+    }
+}
 //# sourceMappingURL=overhide-widgets.d.ts.map
